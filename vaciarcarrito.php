49848c3e33c6b95c5b57f54a61ce485a3f9f4dbb
yahelcontreras <narutete443@gmail.com>
2024-11-15 15:48:22 -0600

    Add files via upload

---
+++ vaciarcarrito.php
@@ -0,0 +1,8 @@
+<?php
+//Aqui se borra el carrito de compras
+Kill ("Carritocompras.txt");
+
+echo "<Center><H2>EL Carrito de compras se ha vaciado</H2></Center>;"
+echo "<BR><BR><A> HREF= Javascript:Window.Close(); >cerrar</A>";
+echo "</Center>;"
+?>
